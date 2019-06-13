e0bf0db96177cfe7b98ce2478f82165093c54b98
selonj <selonj@163.com>
2019-06-13 16:04:59 +0800

    GREEN: make the test pass by add an if statement

--- src/main/java/RomanConverter.java
+++ src/main/java/RomanConverter.java
@@ -4,6 +4,9 @@
  */
 public class RomanConverter {
     public String convert(int n) {
-        return "I";
+        if (n == 1) {
+            return "I";
+        }
+        return "II";
     }
 }
