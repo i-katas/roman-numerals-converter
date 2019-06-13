02392316d51f2ffc556c016e37225642fae2937f
selonj <selonj@163.com>
2019-06-13 20:23:31 +0800

    REFACTOR: optimize memory usage

--- src/main/java/RomanNumeralsConverter.java
+++ src/main/java/RomanNumeralsConverter.java
@@ -6,7 +6,7 @@ public class RomanNumeralsConverter {
     public String convert(int n) {
         StringBuilder outcome = new StringBuilder();
         for (Symbol current : Symbol.values()) {
-            outcome.append(current.computePresentation(n));
+            outcome.append(current.presentationsOf(n));
             n -= current.amount(n);
         }
         return outcome.toString();
@@ -21,13 +21,20 @@ public class RomanNumeralsConverter {
             this.value = value;
         }
 
-        private CharSequence computePresentation(int n) {
+        private CharSequence presentationsOf(int n) {
             int cardinality = cardinality(n);
-            StringBuilder presentation = new StringBuilder(name().length() * cardinality);
-            for (int i = 0; i < cardinality; i++) {
-                presentation.append(name());
+            if (cardinality == 0) {
+                return "";
             }
-            return presentation;
+            return cardinality == 1 ? name() : repeat(cardinality);
+        }
+
+        private CharSequence repeat(int times) {
+            StringBuilder result = new StringBuilder(name().length() * times);
+            for (int i = 0; i < times; i++) {
+                result.append(name());
+            }
+            return result;
         }
 
         private int cardinality(int n) {
