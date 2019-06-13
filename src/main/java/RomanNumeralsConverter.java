ecbeb0f401ebeab9cef69e70f78ca10418b4fa21
selonj <selonj@163.com>
2019-06-13 16:22:41 +0800

    REFACTOR: change all of the conditions

--- src/main/java/RomanNumeralsConverter.java
+++ src/main/java/RomanNumeralsConverter.java
@@ -4,15 +4,15 @@
  */
 public class RomanNumeralsConverter {
     public String convert(int n) {
-        if (n == 5) {
+        if (n >= 5) {
             return "V";
         }
-        if (n == 4) {
+        if (n >= 4) {
             return "IV";
         }
-        if (n > 1) {
+        if (n >= 1) {
             return "I" + convert(n - 1);
         }
-        return "I";
+        return "";
     }
 }
