fe649511b28d8dcc026c126ef1edec068b558699
selonj <selonj@163.com>
2019-06-13 16:45:26 +0800

    GREEN: pass the test adds the rest roman numeral symbols

--- src/main/java/RomanNumeralsConverter.java
+++ src/main/java/RomanNumeralsConverter.java
@@ -13,7 +13,7 @@ public class RomanNumeralsConverter {
     }
 
     private enum Symbol {
-        V(5), IV(4), I(1);
+        M(1000), CM(900), D(500), CD(400), C(100), XC(90), L(50), XL(40), X(10), IX(9), V(5), IV(4), I(1);
 
         private final int value;
 
